fc661341fa1045ef3468f001a8a853225f26aa9f
Achieve-software <vtcbcdtyr@gmail.com>
2024-02-05 03:03:27 +0300

    env

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
+mongoose
+  .connect("mongodb+srv://vtcbcdtyr:vtcbcdtyr@mern-estate.bsiwinw.mongodb.net/mern-estate?retryWrites=true&w=majority")
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
