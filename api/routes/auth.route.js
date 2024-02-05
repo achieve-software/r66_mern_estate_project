b64f4cd890ba7a6a1b2c4f736b051aadbb4e4297
Achieve-software <vtcbcdtyr@gmail.com>
2024-02-05 04:00:19 +0300

    signup

---
+++ api/routes/auth.route.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post('/google', google);
+router.get('/signout', signOut)
+
+export default router;
