089a55a8f32d0534f0d38eb4925e9cc12c3ae09a
Achieve-software <vtcbcdtyr@gmail.com>
2024-02-08 17:17:35 +0300

    ss

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -27,11 +27,13 @@ export default function SignUp() {
           
         />
 
+
+
 <button
            
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sign up
+        sign up
         </button>
         
       
