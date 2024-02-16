723f5152a3f512552a9c63521db39ecf093067df
Achieve-software <vtcbcdtyr@gmail.com>
2024-02-16 18:01:32 +0300

    Sgn

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -39,7 +39,7 @@ export default function SignUp() {
       
       </form>
       <div className='flex gap-2 mt-5'>
-        <p>Have an account?</p>
+        <p>Have AN account?</p>
         <Link to={'/sign-in'}>
           <span className='text-blue-700'>Sign in</span>
         </Link>
